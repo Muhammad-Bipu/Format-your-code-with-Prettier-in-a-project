dbb9814015408d3cf716fd677120d6871bf17aa3
Muhammad-Bipu <mdhasanbipu@gmail.com>
2025-08-15 22:26:16 +0600

    Format your code with Prettier in a project

---
+++ script.js
@@ -0,0 +1,18 @@
+const firstName = 'Muhammad'
+const lastName = 'Bipu'
+const fullName = firstName + lastName
+
+console.log('hello')
+
+if (firstName === 'Muhammad') {
+  console.log(firstName)
+}
+
+const user = {
+  name: 'Muhammad Bipu',
+  age: 25
+}
+
+const printDetails = name => {
+  console.log(name)
+}
